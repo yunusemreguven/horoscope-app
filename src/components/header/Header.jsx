38181de33f0e8efa08ecece83a6998203140ede9
Yunus Emre Güven <emreguven.yg@gmail.com>
2023-07-27 18:03:48 +0200

    first commit

---
+++ src/components/header/Header.jsx
@@ -0,0 +1,13 @@
+import "./Header.scss";
+
+const Header = () => {
+  return (
+    <div>
+      <div className="header">
+        <span className="text-shadow">WHO ARE YOU?</span>
+      </div>
+    </div>
+  );
+};
+
+export default Header;
